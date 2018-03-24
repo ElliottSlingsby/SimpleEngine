bf47638c275877620df301e85f9400d0fc371fb8
Elliott Slingsby <elliottslingsby@googlemail.com>
2018-03-24 01:42:10 +0000

    Removed realloc (component pointers never move now)

--- include/ObjectPool.hpp
+++ include/ObjectPool.hpp
@@ -5,15 +5,14 @@
 #include <cassert>
 #include <utility>
 
+#include <vector>
+
 class BasePool {
 protected:
 	const size_t _chunkSize;
-	const size_t _objectSize;
+	const size_t _elementSize;
 
-	uint8_t* _buffer = nullptr;
-	size_t _size = 0;
-
-	inline uint8_t* _conv(void* memory) const;
+	std::vector<uint8_t*> _chunks;
 
 public:
 	inline BasePool(size_t elementSize, size_t chunkSize);
@@ -28,8 +27,6 @@ public:
 	template <typename T, typename ...Ts>
 	inline void insert(uint32_t index, Ts&&... args);
 
-	inline size_t size() const;
-
 	inline uint32_t count() const;
 
 	virtual inline void erase(uint32_t index) = 0;
@@ -43,56 +40,57 @@ public:
 	inline void erase(uint32_t index) override;
 };
 
-BasePool::BasePool(size_t elementSize, size_t chunkSize) : _objectSize(elementSize), _chunkSize(chunkSize) {
-	reserve(0);
-}
+BasePool::BasePool(size_t elementSize, size_t chunkSize) : _elementSize(elementSize), _chunkSize(chunkSize) {}
 
 BasePool::~BasePool() {
-	free(_buffer);
+	for (uint8_t* chunk : _chunks)
+		free(chunk);
 }
 
 void BasePool::reserve(uint32_t index) {
-	if (index > count())
+	if (index < count())
 		return;
 
-	_size = (index * _objectSize) + _chunkSize - ((index * _objectSize) % _chunkSize);
+	size_t elementsPerChunk = _chunkSize / _elementSize;
 
-	if (_buffer == nullptr)
-		_buffer = _conv(malloc(_size));
-	else
-		_buffer = _conv(realloc(_buffer, _size));
-}
+	uint32_t chunk = static_cast<uint32_t>(index / elementsPerChunk);
 
-uint8_t* BasePool::_conv(void* memory) const {
-	assert(memory != nullptr);
+	size_t size = _chunks.size();
+	_chunks.resize(chunk + 1);
 
-	return static_cast<uint8_t*>(memory);
+	for (size_t i = size; i < chunk + 1; i++) {
+		_chunks[i] = static_cast<uint8_t*>(malloc(_chunkSize));
+		assert(_chunks[i]);
+	}
 }
 
 template<typename T>
 T* BasePool::get(uint32_t index) {
-	assert(sizeof(T) <= _objectSize);
+	assert(sizeof(T) <= _elementSize);
 	assert(index < count());
 
-	return reinterpret_cast<T*>(_buffer + (index * _objectSize));
+	size_t elementsPerChunk = _chunkSize / _elementSize;
+
+	uint32_t chunk = static_cast<uint32_t>(index / elementsPerChunk);
+
+	size_t offset = (index - (chunk * elementsPerChunk)) * _elementSize;
+
+	return reinterpret_cast<T*>(_chunks[chunk] + offset);
 }
 
 template <typename T, typename ...Ts>
 void BasePool::insert(uint32_t index, Ts&&... args) {
-	assert(sizeof(T) <= _objectSize);
+	assert(sizeof(T) <= _elementSize);
 
 	if (index >= count())
 		reserve(index);
 
-	new(static_cast<void*>(_buffer + (index * _objectSize))) T(std::forward<Ts>(args)...);
-}
-
-size_t BasePool::size() const {
-	return _size;
+	new(static_cast<void*>(get<T*>(index))) T(std::forward<Ts>(args)...);
 }
 
 uint32_t BasePool::count() const {
-	return static_cast<uint32_t>(_size / _objectSize);
+	size_t elementsPerChunk = _chunkSize / _elementSize;
+	return static_cast<uint32_t>(_chunks.size() * elementsPerChunk);
 }
 
 template<class T>
